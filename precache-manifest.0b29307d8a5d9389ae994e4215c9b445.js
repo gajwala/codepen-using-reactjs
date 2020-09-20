5bf63be586e43c1bcc8698feb0ec6ffc6950629a
narayana bathi <narayanabathi@narayanas-MacBook-Pro.local>
2020-09-20 14:55:37 +0530

    Updates

---
+++ precache-manifest.0b29307d8a5d9389ae994e4215c9b445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40f64f0f76e5de276fed38f5517765c",
+    "url": "/codepen-using-reactjs/index.html"
+  },
+  {
+    "revision": "ab408cee5306d61696d1",
+    "url": "/codepen-using-reactjs/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "2259b1259728832a7ab1",
+    "url": "/codepen-using-reactjs/static/css/main.3d95d3d5.chunk.css"
+  },
+  {
+    "revision": "ab408cee5306d61696d1",
+    "url": "/codepen-using-reactjs/static/js/2.57ea58c5.chunk.js"
+  },
+  {
+    "revision": "a168c24010f99fb13adeefd582258e3b",
+    "url": "/codepen-using-reactjs/static/js/2.57ea58c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2259b1259728832a7ab1",
+    "url": "/codepen-using-reactjs/static/js/main.37a76aa1.chunk.js"
+  },
+  {
+    "revision": "d371b592a301aeed2541",
+    "url": "/codepen-using-reactjs/static/js/runtime-main.83d7a5d4.js"
+  }
+]);
